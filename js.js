c14a0024b55a08b7f8f1bb1ef01b5e9badc88229
tiagooogomes <91339622+tiagooogomes@users.noreply.github.com>
2021-11-22 05:05:33 -0300

    Primeiro

---
+++ js.js
@@ -0,0 +1,78 @@
+const h1 = document.querySelector('#titulo');
+h1.textContent = 'BRITNEY';
+
+const h01 = document.querySelector('#titulo2');
+h01.textContent = 'SPEARS';
+
+const h2 = document.querySelector('#sobre');
+h2.textContent = 'SOBRE';
+
+const p = document.querySelector('#texto');
+p.textContent = 'Britney Jean Spears (McComb, 2 de dezembro de 1981) é uma cantora, compositora, dançarina e atriz norte-americana. Nascida no Mississippi e criada em Kentwood, no estado de Louisiana, ela iniciou sua carreira artística atuando em papeis em produções teatrais e programas de televisão durante a infância, antes de assinar um contrato com a Jive Records em 1997. Seu primeiro e segundo álbuns de estúdio, ...Baby One More Time (1999) e Oops!... I Did It Again (2000), tornaram-se sucessos internacionais, com o primeiro tornando-se o álbum mais vendido por uma artista solo adolescente.';
+
+const gif = document.querySelector('#gif');
+gif.setAttribute('src', 'img/sobre2.jpg');
+
+const nome = document.querySelector('#nome');
+nome.textContent = 'Tiago Santos';
+
+const idade = document.querySelector('#idade');
+idade.textContent = 'Quantos anos?';
+
+const vinte = document.querySelector('#vinte');
+vinte.textContent = '20';
+
+const estado = document.querySelector('#estado');
+estado.textContent = 'De qual estado?';
+
+const ba = document.querySelector('#ba');
+ba.textContent = 'BA';
+
+const fa = document.querySelector('#fa');
+fa.textContent = 'Fã?';
+
+const muito = document.querySelector('#muito');
+muito.textContent = 'Super';
+
+const foto = document.querySelector('#foto');
+foto.setAttribute('src', 'img/eu.jpg');
+
+const preferidas = document.querySelector('#preferidas');
+preferidas.textContent = 'MELHORES MÚSICAS';
+
+const musica = document.querySelector('#musica');
+musica.setAttribute('src', 'img/musica.jpg');
+
+const musica2 = document.querySelector('#musica2');
+musica2.setAttribute('src', 'img/img2.jpg');
+
+const musica3 = document.querySelector('#musica3');
+musica3.setAttribute('src', 'img/img3.jpg');
+
+const h3 = document.querySelector('#h3');
+h3.textContent = 'I´m not a girl not yet a woman';
+
+const toxie = document.querySelector('#toxie');
+toxie.textContent = 'Toxic';
+
+const oops = document.querySelector('#oops');
+oops.textContent = 'Oops!...I did it again';
+
+const data = document.querySelector('#data');
+data.textContent = '2009';
+
+const data2 = document.querySelector('#data2');
+data2.textContent = '2009';
+
+const data3 = document.querySelector('#data3');
+data3.textContent = '2009';
+
+const assistir = document.querySelector('#assistir');
+assistir.textContent = 'Escutar';
+
+const assistir2 = document.querySelector('#assistir2');
+assistir2.textContent = 'Escutar';
+
+const assistir3 = document.querySelector('#assistir3');
+assistir3.textContent = 'Escutar';
+
